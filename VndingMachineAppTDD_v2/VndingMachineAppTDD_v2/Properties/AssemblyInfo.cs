5cec3d2e60f5aa4581022370525dae27e5f6baed
movovijay05 <movovijay05@gmail.com>
2017-06-08 10:44:03 -0400

    Craeted a new test project

---
+++ VndingMachineAppTDD_v2/VndingMachineAppTDD_v2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VndingMachineAppTDD_v2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nationwide")]
+[assembly: AssemblyProduct("VndingMachineAppTDD_v2")]
+[assembly: AssemblyCopyright("Copyright © Nationwide 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6581c292-55cb-4841-9258-d3a9400ee979")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
