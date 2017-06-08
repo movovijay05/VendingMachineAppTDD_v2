b66ec65202611a2305057648c73c4bcdd9d4f13f
movovijay05 <movovijay05@gmail.com>
2017-06-08 12:11:00 -0400

    Created a Coin validator class and added methods to do all coin validations. Added tests to validate the Coin validation methods

---
+++ VndingMachineAppTDD_v2/VndingMachineAppTDD_v2/Models/CoinValidator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VndingMachineAppTDD_v2.Constants;
+
+namespace VndingMachineAppTDD_v2.Models
+{
+    class CoinValidator
+    {
+        public Boolean isValidCoinType(int coinType)
+        {
+            Boolean isValidCoin = false;
+            switch (coinType)
+            {
+                case 2:
+                    Console.WriteLine(CoinDetailsEnum.NickelsName);
+                    isValidCoin = true;
+                    break;
+                case 3:
+                    Console.WriteLine(CoinDetailsEnum.DimesName);
+                    isValidCoin = true;
+                    break;
+                case 4:
+                    Console.WriteLine(CoinDetailsEnum.QuartersName);
+                    isValidCoin = true;
+                    break;
+            }
+            return isValidCoin;
+        }
+
+        public Boolean isValidCoinDimensions(double coinDimension, string dimensionName, string metricSystem)
+        {
+            Boolean isValidCoinDimensions = false;
+            switch (dimensionName)
+            {
+                case "Diameter":
+                    if ((metricSystem == CoinDetailsEnum.DiameterMetric) && (CoinDetailsEnum.acceptable_diameter.Contains(coinDimension)))
+                    {
+                        isValidCoinDimensions = true;
+                    }
+                    break;
+                case "Weight":
+                    if ((metricSystem == CoinDetailsEnum.WeightMetric) && (CoinDetailsEnum.acceptable_weight.Contains(coinDimension)))
+                    {
+                        isValidCoinDimensions = true;
+                    }
+                    break;
+                case "Thickness":
+                    if ((metricSystem == CoinDetailsEnum.ThicknessMetric) && (CoinDetailsEnum.acceptable_thickness.Contains(coinDimension)))
+                    {
+                        isValidCoinDimensions = true;
+                    }
+                    break;
+            }
+            return isValidCoinDimensions;
+        }
+    }
+}
--- VndingMachineAppTDD_v2/VndingMachineAppTDD_v2/VendingMachineAppTests.cs
+++ VndingMachineAppTDD_v2/VndingMachineAppTDD_v2/VendingMachineAppTests.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VndingMachineAppTDD_v2.Models;
 
-namespace VndingMachineAppTDD_v2
+namespace VendingMachineAppTDD_v2
 {
     [TestClass]
     public class VendingMachineAppTests
     {
+        CoinValidator cv = new CoinValidator();
         [TestMethod]
-        public void TestMethod1()
+        public void testifVMAcceptsOnlyNickelsDimesAndQuarters()
         {
+            Assert.IsTrue(cv.isValidCoinType(2));
+            Assert.IsTrue(cv.isValidCoinType(3));
+            Assert.IsTrue(cv.isValidCoinType(4));
+        }
+        [TestMethod]
+        public void testifVMAcceptsOnlyCoinsWithValidDimensions()
+        {
+            Assert.IsTrue(cv.isValidCoinDimensions(5.00, "Weight", "grams"));
+            Assert.IsTrue(cv.isValidCoinDimensions(1.75, "Thickness", "mm"));
+            Assert.IsTrue(cv.isValidCoinDimensions(0.71, "Diameter", "inches"));
         }
     }
 }
