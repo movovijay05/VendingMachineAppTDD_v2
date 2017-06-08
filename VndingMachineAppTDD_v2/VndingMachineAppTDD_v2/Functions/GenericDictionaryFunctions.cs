e0338cf533c95a4ef046cc8d08a2de12bdaf5793
movovijay05 <movovijay05@gmail.com>
2017-06-08 16:35:13 -0400

    Testing generic functions

--- VndingMachineAppTDD_v2/VndingMachineAppTDD_v2/Functions/GenericDictionaryFunctions.cs
+++ VndingMachineAppTDD_v2/VndingMachineAppTDD_v2/Functions/GenericDictionaryFunctions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,8 +18,32 @@ namespace VndingMachineAppTDD_v2.Functions
             }
             dictionaryStringOutput = dictionaryStringOutput.Replace("[", "")
                 .Replace("]", "")
-                .Replace(",", ":");
+                .Replace(" ", "")
+                .Replace(",", ":")
+                .Replace("\n", ":");
+            Trace.WriteLine(dictionaryStringOutput);
             return dictionaryStringOutput;
+        }
+
+      public bool checkIfTwoStringIntDictionariesAreIdenticalWithoutSorting(
+      Dictionary<string, int> oldDict,
+      Dictionary<string, int> newDict)
+        {
+            // Simple check, are the counts the same?
+            if (!oldDict.Count.Equals(newDict.Count)) return false;
+
+            // iterate through all the keys in oldDict and
+            // verify whether the key exists in the newDict
+            foreach (int i in Enumerable.Range(0, oldDict.Count - 1))
+            {
+                if (newDict.ElementAt(i).Equals(oldDict.ElementAt(i)))
+                {
+                    // iterate through each value for the current key in oldDict and 
+                    // verify whether or not it exists for the current key in the newDict
+                }
+                else { return false; }
+            }
+            return true;
         }
     }
 }
